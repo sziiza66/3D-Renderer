7bc8c4ad71c313816246e9c5d97f293293482f91
sziiza66 <iv.lin6wrk@gmail.com>
2025-01-26 19:16:18 +0300

    Fixed rasterized bug, now Zbuffer works fine.

--- src/model/rasterizer.cpp
+++ src/model/rasterizer.cpp
@@ -5,6 +5,8 @@
 namespace Renderer3D::Kernel {
 namespace {
 
+constexpr double SIDEOFTHECUBE = 2.0;
+
 bool IsTriangleInTheFrustrum(const Renderer3D::Kernel::Triangle& triangle) {
     return abs(triangle(2, 0)) < 1 && abs(triangle(2, 1)) < 1 && abs(triangle(2, 2)) < 1;
 }
@@ -23,10 +25,18 @@ std::tuple<uint8_t, uint8_t, uint8_t> GetVerticalOrderOfVertices(const Renderer3
     return ret;
 }
 
-inline void FillLine(const Triangle& triangle, Frame& frame, std::vector<double>& z_buffer_, size_t x, size_t y1,
-                     size_t y2, double real_z, double z_diff_y) {
+inline void FillLine(const Triangle& triangle, Frame& frame, std::vector<double>& z_buffer_, size_t x, double real_y1,
+                     double real_y2, double real_z, double z_diff_y, double real_z_diff_y) {
+
     size_t edge = frame.GetWidth() - 1;
-    for (size_t y = (y1 < 0 ? 0 : y1); y <= (y2 >= edge ? edge : y2); ++y, real_z += z_diff_y) {
+    if (real_y1 < -1) {
+        real_z += (-1 - real_y1) * real_z_diff_y;
+        real_y1 = -1;
+    }
+    size_t y1 = (real_y1 + 1) / SIDEOFTHECUBE * frame.GetWidth();
+    size_t y2 = (real_y2 + 1) / SIDEOFTHECUBE * frame.GetWidth();
+
+    for (size_t y = y1; y <= (y2 >= edge ? edge : y2); ++y, real_z += z_diff_y) {
         if (real_z < z_buffer_[x * frame.GetWidth() + y]) {
             z_buffer_[x * frame.GetWidth() + y] = real_z;
             frame(x, y) = triangle.GetColor();
@@ -38,27 +48,28 @@ inline void FillLowerTriangle(const Triangle& triangle, Frame& frame, std::vecto
                               uint8_t middle, uint8_t highest, double real_z_diff_y, double z_diff_y, double z_diff_x,
                               double& real_x, double& real_z, size_t& x, double& prev_y) {
     double mid_x = (triangle(0, middle) <= 1 ? triangle(0, middle) : 1);
-    double dx = 2.0 / frame.GetHeight();
+    double dx = SIDEOFTHECUBE / frame.GetHeight();
     for (; real_x < mid_x; ++x, real_x += dx, real_z += z_diff_x) {
         double real_y1 = triangle(1, lowest) + (triangle(1, highest) - triangle(1, lowest)) *
                                                    (real_x - triangle(0, lowest)) /
                                                    (triangle(0, highest) - triangle(0, lowest));
-        size_t y1 = ((real_y1 < -1 ? -1 : real_y1) + 1) / 2 * frame.GetWidth();
         double real_y2 =
             triangle(0, lowest) == triangle(0, middle)
                 ? triangle(1, middle)
                 : (triangle(1, lowest) + (triangle(1, middle) - triangle(1, lowest)) * (real_x - triangle(0, lowest)) /
                                              (triangle(0, middle) - triangle(0, lowest)));
-        size_t y2 = ((real_y2 < -1 ? -1 : real_y2) + 1) / 2 * frame.GetWidth();
 
         if (real_y1 > real_y2) {
-            std::swap(y1, y2);
             std::swap(real_y1, real_y2);
         }
+        if (real_y2 < -1) {
+            continue;
+        }
+
         real_z += real_z_diff_y * (real_y1 - prev_y);
         prev_y = real_y1;
 
-        FillLine(triangle, frame, z_buffer_, x, y1, y2, real_z, z_diff_y);
+        FillLine(triangle, frame, z_buffer_, x, real_y1, real_y2, real_z, z_diff_y, real_z_diff_y);
     }
 }
 
@@ -66,26 +77,27 @@ inline void FillUpperTriangle(const Triangle& triangle, Frame& frame, std::vecto
                               uint8_t middle, uint8_t highest, double real_z_diff_y, double z_diff_y, double z_diff_x,
                               double& real_x, double& real_z, size_t& x, double& prev_y) {
     double top_x = (triangle(0, highest) <= 1 ? triangle(0, highest) : 1);
-    for (; real_x < top_x; ++x, real_x += 2.0 / frame.GetHeight(), real_z += z_diff_x) {
+    double dx = SIDEOFTHECUBE / frame.GetHeight();
+    for (; real_x < top_x; ++x, real_x += dx, real_z += z_diff_x) {
         double real_y1 = triangle(1, lowest) + (triangle(1, highest) - triangle(1, lowest)) *
                                                    (real_x - triangle(0, lowest)) /
                                                    (triangle(0, highest) - triangle(0, lowest));
-        size_t y1 = ((real_y1 < -1 ? -1 : real_y1) + 1) / 2 * frame.GetWidth();
         double real_y2 = (triangle(0, highest) == triangle(0, middle)
                               ? triangle(1, highest)
                               : (triangle(1, middle) + (triangle(1, highest) - triangle(1, middle)) *
                                                            (real_x - triangle(0, middle)) /
                                                            (triangle(0, highest) - triangle(0, middle))));
-        size_t y2 = ((real_y2 < -1 ? -1 : real_y2) + 1) / 2 * frame.GetWidth();
-
         if (real_y1 > real_y2) {
-            std::swap(y1, y2);
             std::swap(real_y1, real_y2);
         }
+        if (real_y2 < -1) {
+            continue;
+        }
+
         real_z += real_z_diff_y * (real_y1 - prev_y);
         prev_y = real_y1;
 
-        FillLine(triangle, frame, z_buffer_, x, y1, y2, real_z, z_diff_y);
+        FillLine(triangle, frame, z_buffer_, x, real_y1, real_y2, real_z, z_diff_y, real_z_diff_y);
     }
 }
 
@@ -94,6 +106,7 @@ void DrawTriangle(Frame& frame, std::vector<double>& z_buffer_, const Triangle& 
         return;
     }
 
+    // Индексы вершин треугольника, отсортированные по Ox.
     auto [lowest, middle, highest] = GetVerticalOrderOfVertices(triangle);
 
     // Получаю векторы, напраленные из нижней (по Ox) точки треугольника в среднюю и верхнюю соотв.
@@ -113,14 +126,14 @@ void DrawTriangle(Frame& frame, std::vector<double>& z_buffer_, const Triangle& 
     // real_x, real_y, real_z -- координаты пооддреживаемой точки в видимом прстранстве.
 
     double real_z_diff_y = (v1(2) == 0 ? 0 : -v1(1) / v1(2));
-    double z_diff_y = real_z_diff_y * 2 / frame.GetWidth();
-    double z_diff_x = (v1(2) == 0 ? 0 : -v1(0) / v1(2) * 2 / frame.GetHeight());
+    double z_diff_y = real_z_diff_y * SIDEOFTHECUBE / frame.GetWidth();
+    double z_diff_x = (v1(2) == 0 ? 0 : -v1(0) / v1(2) * SIDEOFTHECUBE / frame.GetHeight());
 
     double real_x = (triangle(0, lowest) >= -1 ? triangle(0, lowest) : -1);
     double prev_y = triangle(1, lowest);
     double real_z = triangle(2, lowest) - v1(0) / v1(2) * (real_x - triangle(0, lowest));
 
-    size_t x = (real_x + 1) / 2 * frame.GetHeight();
+    size_t x = (real_x + 1) / SIDEOFTHECUBE * frame.GetHeight();
 
     FillLowerTriangle(triangle, frame, z_buffer_, lowest, middle, highest, real_z_diff_y, z_diff_y, z_diff_x, real_x,
                       real_z, x, prev_y);
@@ -136,6 +149,9 @@ Frame BufferRasterizer::MakeFrame(const std::vector<Triangle>& triangles, Frame&
     Frame ret(std::move(frame));
     ret.Clear();
     for (const auto& triangle : triangles) {
+        // Т.к. я пока что не реализовал клиппинг, логика растерайзера обрабатывает случаи, когда вершины трегольника
+        // выходят за границы видимой зоны, когда я реализую клиппинг, эту лишнюю логику можно будет убрать, ну или хотя
+        // бы упростить.
         DrawTriangle(ret, z_buffer, triangle);
     }
     return ret;
